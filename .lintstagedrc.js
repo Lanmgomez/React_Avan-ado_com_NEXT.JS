c5604d596e283eb9b04aaf055c4e3af54d135d8f
Lanmgomez <islan.gomes@supplylabs.com.br>
2024-11-06 18:40:51 -0300

    testando...

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,6 +1,7 @@
 module.exports = {
   '*.{js,jsx,ts,tsx}': (filenames) => [
     `prettier --write ${filenames.join(' ')}`,
-    `eslint ${filenames.join(' ')}`
+    `eslint ${filenames.join(' ')}`,
+    `npm test -- --findRelatedTests ${filenames.join(' ')}`
   ]
 }
